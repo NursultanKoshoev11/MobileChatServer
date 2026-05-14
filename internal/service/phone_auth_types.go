8d319d0a7b461257355874f34560bac65ea55bdf
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-05-14 19:50:02 +0600

    Add phone auth request types

---
+++ internal/service/phone_auth_types.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"regexp"
+	"strings"
+)
+
+var phonePattern = regexp.MustCompile(`^\+[1-9][0-9]{7,14}$`)
+
+type RequestPhoneCodeInput struct {
+	Mobile string `json:"mobile"`
+}
+
+type VerifyPhoneCodeInput struct {
+	Mobile      string `json:"mobile"`
+	Code        string `json:"code"`
+	DisplayName string `json:"display_name"`
+}
+
+type RequestPhoneCodeOutput struct {
+	Status  string `json:"status"`
+	DevCode string `json:"dev_code,omitempty"`
+}
+
+func normalizeMobile(raw string) (string, error) {
+	mobile := strings.ReplaceAll(strings.TrimSpace(raw), " ", "")
+	mobile = strings.ReplaceAll(mobile, "-", "")
+	mobile = strings.ReplaceAll(mobile, "(", "")
+	mobile = strings.ReplaceAll(mobile, ")", "")
+	if !phonePattern.MatchString(mobile) {
+		return "", NewValidationError("mobile must be in international format, for example +996700123456")
+	}
+	return mobile, nil
+}
