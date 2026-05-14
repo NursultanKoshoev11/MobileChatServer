4d88639efd9d4b1eda5236bc38b7be13a56804aa
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-05-14 19:33:13 +0600

    Add opaque refresh token helpers

---
+++ internal/security/tokens.go
@@ -0,0 +1,21 @@
+package security
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+)
+
+func NewOpaqueToken(byteCount int) (string, error) {
+	buf := make([]byte, byteCount)
+	if _, err := rand.Read(buf); err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(buf), nil
+}
+
+func HashToken(token string) string {
+	sum := sha256.Sum256([]byte(token))
+	return hex.EncodeToString(sum[:])
+}
