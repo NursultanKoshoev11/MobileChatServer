b2cd58cca43d8261aaaf1b1c059b88f7edf156f2
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-05-14 19:47:59 +0600

    Add phone auth domain models

---
+++ internal/domain/phone_auth.go
@@ -0,0 +1,16 @@
+package domain
+
+import "time"
+
+type PhoneAuthUser struct {
+	ID          string    `json:"id"`
+	Mobile      string    `json:"mobile"`
+	DisplayName string    `json:"display_name"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type PhoneSession struct {
+	AccessToken  string        `json:"access_token"`
+	RefreshToken string        `json:"refresh_token"`
+	User         PhoneAuthUser `json:"user"`
+}
